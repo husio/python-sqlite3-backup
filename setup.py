986d7adc7e0db07e4f044a3a13de42ac8bdefd06
Piotr Husiatyński <phusiatynski@gmail.com>
2012-03-28 12:09:35 +0200

    setup.py updated (now on PYPI)

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-
+import os
 from distutils.core import setup, Extension
 
 module = Extension('sqlitebck',
         sources=['src/sqlitebck.c', ],
         libraries=['sqlite3', ])
 
+def long_description():
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fd:
+        return fd.read()
+
 setup(name='sqlitebck',
     version='1.0',
     include_dirs=['/usr/local/include', ],
     description='Sqlite3 online backup API implementation.',
+    long_description=long_description(),
+    url='https://github.com/husio/husio.github.com',
+    download_url='',
     ext_modules=[module, ],
     author='Piotr Husiatyński',
     author_email='phusiatynski@gmail.com',
@@ -20,6 +26,8 @@ setup(name='sqlitebck',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
-        'Programming Language :: SQL',
-        'Programming Language :: Python',
+        'Topic :: Database',
+        'Programming Language :: C',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
     ])
